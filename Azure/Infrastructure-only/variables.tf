c20048c9af1dfbc31b4b8a7b5533acc94077f1c1
Gage Kulland <g.kulland@f5.com>
2023-09-05 08:09:48 -0700

    Moved all variables values to terraform.tfvars file

--- Azure/Infrastructure-only/variables.tf
+++ Azure/Infrastructure-only/variables.tf
@@ -1,43 +1,44 @@
+#### NOTE ####
+# All variables are defined in the variables.tf file.  
+#This file is used to define the variables and their descriptions.  
+#The values for the variables are defined in the terraform.tfvars file.  
+#The terraform.tfvars file is not included in this repository for security reasons.  
+#The terraform.tfvars file should be created in the same directory as the variables.tf file.  
+#The terraform.tfvars file should contain the following variables and their values.
+#### End of NOTE ####
+
 # Variables
 
 variable "projectPrefix" {
   type        = string
-  default     = "Kulland-HA-ALB-"
   description = "This value is inserted at the beginning of each Azure object (alpha-numeric, no special character)"
 }
 variable "location" {
   type        = string
-  default     = "westus2"
   description = "Azure Location of the deployment"
 }
 variable "adminSrcAddr" {
   type        = string
   description = "Allowed Admin source IP prefix"
-  default     = "65.61.116.107/32"
 }
 variable "vnet_cidr" {
   type        = string
-  default     = "10.245.0.0/16"
   description = "CIDR IP Address range of the Virtual Network"
 }
 variable "mgmt_address_prefix" {
   type        = string
-  default     = "10.245.1.0/24"
   description = "Management subnet address prefix"
 }
 variable "ext_address_prefix" {
   type        = string
-  default     = "10.245.2.0/24"
   description = "External subnet address prefix"
 }
 variable "int_address_prefix" {
   type        = string
-  default     = "10.245.3.0/24"
   description = "Internal subnet address prefix"
 }
 variable "resourceOwner" {
   type        = string
-  default     = "kulland"
   description = "This is a tag used for object creation. Example is last name."
 }
 variable "f5_cloud_failover_label" {
@@ -48,4 +49,16 @@ variable "f5_cloud_failover_label" {
 variable "client_secret" {
   type        = string
   description = "This is the client secret for the Service Principal"
+}
+variable "client_id" {
+  type        = string
+  description = "This is the client id for the Service Principal"
+}
+variable "tenant_id" {
+  type        = string
+  description = "This is the tenant id for the Service Principal"
+}
+variable "subscription_id" {
+  type        = string
+  description = "This is the subscription id for the Service Principal"
 }
