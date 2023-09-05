7d3798b0dff6290b44ca22d40514b04677042b43
Gage Kulland <g.kulland@f5.com>
2023-09-05 07:46:49 -0700

    Modified some of the Infrastructure files. Modified the terraform.tfvars.example file in the Infrastructure folder to terraform.tfvars. Added service principal details to the main.tf file in the Infrastructure folder.

--- Azure/Infrastructure-only/variables.tf
+++ Azure/Infrastructure-only/variables.tf
@@ -2,7 +2,7 @@
 
 variable "projectPrefix" {
   type        = string
-  default     = "demo"
+  default     = "Kulland-HA-ALB-"
   description = "This value is inserted at the beginning of each Azure object (alpha-numeric, no special character)"
 }
 variable "location" {
@@ -13,35 +13,39 @@ variable "location" {
 variable "adminSrcAddr" {
   type        = string
   description = "Allowed Admin source IP prefix"
-  default     = "0.0.0.0/0"
+  default     = "65.61.116.107/32"
 }
 variable "vnet_cidr" {
   type        = string
-  default     = "10.90.0.0/16"
+  default     = "10.245.0.0/16"
   description = "CIDR IP Address range of the Virtual Network"
 }
 variable "mgmt_address_prefix" {
   type        = string
-  default     = "10.90.1.0/24"
+  default     = "10.245.1.0/24"
   description = "Management subnet address prefix"
 }
 variable "ext_address_prefix" {
   type        = string
-  default     = "10.90.2.0/24"
+  default     = "10.245.2.0/24"
   description = "External subnet address prefix"
 }
 variable "int_address_prefix" {
   type        = string
-  default     = "10.90.3.0/24"
+  default     = "10.245.3.0/24"
   description = "Internal subnet address prefix"
 }
 variable "resourceOwner" {
   type        = string
-  default     = null
+  default     = "kulland"
   description = "This is a tag used for object creation. Example is last name."
 }
 variable "f5_cloud_failover_label" {
   type        = string
   default     = "myFailover"
   description = "This is a tag used for F5 Cloud Failover Extension to identity which cloud objects to move during a failover event."
+}
+variable "client_secret" {
+  type        = string
+  description = "This is the client secret for the Service Principal"
 }
