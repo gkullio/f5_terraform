1ec749222dc08c98c30c9b41c4d515fd978d04c2
Gage Kulland <g.kulland@f5.com>
2023-09-20 06:38:43 -0700

    Folder Rename

--- XC/xc/http_loadbalancer.tf
+++ XC/xc/http_loadbalancer.tf
@@ -9,7 +9,7 @@ resource "volterra_origin_pool" "origin" {
   origin_servers {
 
     public_name {
-      dns_name = "httpbin.org"
+      dns_name = "coleman.myedgedemo.com"
     }
 
     labels = {
@@ -176,70 +176,7 @@ resource "volterra_http_loadbalancer" "appProxy" {
     }
   }
 
-  routes {
-    redirect_route {
-      http_method = "ANY"
-      path {
-        path = "/"
-      }
-      headers {
-        name         = "Accept-Language"
-        regex        = "(.*[fF][rR]-[fF][rR].*$)"
-        invert_match = false
-      }
-      route_redirect {
-        proto_redirect    = "incoming-proto"
-        host_redirect     = "${var.name}.${var.delegated_dns_domain}"
-        path_redirect     = "/fr/"
-        response_code     = "301"
-        retain_all_params = true
-        port_redirect     = 0
-      }
-    }
-  }
-  routes {
-    redirect_route {
-      http_method = "ANY"
-      path {
-        path = "/"
-      }
-      headers {
-        name         = "Accept-Language"
-        regex        = "(.*[pP][tT]-[bB][rR].*)"
-        invert_match = false
-      }
-      route_redirect {
-        proto_redirect    = "incoming-proto"
-        host_redirect     = "${var.name}.${var.delegated_dns_domain}"
-        path_redirect     = "/pt/"
-        response_code     = "301"
-        retain_all_params = true
-        port_redirect     = 0
-      }
-    }
-  }
-  routes {
-    redirect_route {
-      http_method = "ANY"
-      path {
-        path = "/"
-      }
-      headers {
-        name         = "Accept-Language"
-        regex        = "(.*[eE][sS]-[0-9].*$)"
-        invert_match = false
-      }
-      route_redirect {
-        proto_redirect    = "incoming-proto"
-        host_redirect     = "${var.name}.${var.delegated_dns_domain}"
-        path_redirect     = "/es/"
-        response_code     = "301"
-        retain_all_params = true
-        port_redirect     = 0
-      }
-    }
-  }
-  routes {
+ routes {
     simple_route {
       http_method = "ANY"
       path {
@@ -253,7 +190,6 @@ resource "volterra_http_loadbalancer" "appProxy" {
         weight   = 1
         priority = 1
       }
-      host_rewrite = "httpbin.org"
     }
   }
   routes {
@@ -269,27 +205,6 @@ resource "volterra_http_loadbalancer" "appProxy" {
         }
         weight   = 1
         priority = 1
-      }
-    }
-  }
-  routes {
-    redirect_route {
-      http_method = "ANY"
-      path {
-        path = "/"
-      }
-      headers {
-        name         = "Accept-Language"
-        regex        = "(.*[eE][nN]-[uU][sS].*$)"
-        invert_match = false
-      }
-      route_redirect {
-        proto_redirect    = "incoming-proto"
-        host_redirect     = "${var.name}.${var.delegated_dns_domain}"
-        path_redirect     = "/en-us/"
-        response_code     = "301"
-        retain_all_params = true
-        port_redirect     = 0
       }
     }
   }
@@ -325,104 +240,4 @@ resource "volterra_http_loadbalancer" "appProxy" {
       }
     }
   }
-
-  routes {
-    simple_route {
-      http_method = "ANY"
-      path {
-        prefix = "/written/"
-      }
-      origin_pools {
-        pool {
-          namespace = var.namespace
-          name      = volterra_origin_pool.origin.name
-        }
-        weight   = 1
-        priority = 1
-      }
-      advanced_options {
-        priority       = "DEFAULT"
-        prefix_rewrite = "/rewritten/"
-        app_firewall {
-          name      = "${var.name}-waap"
-          namespace = var.namespace
-        }
-      }
-    }
-  }
-  routes {
-    direct_response_route {
-      http_method = "ANY"
-      path {
-        prefix = "/thanks"
-      }
-      route_direct_response {
-        response_code = "200"
-        response_body = "You're Welcome."
-      }
-    }
-  }
-  routes {
-    custom_route_object {
-      route_ref {
-        namespace = var.namespace
-        name      = "${var.name}-custom-route"
-      }
-    }
-  }
-
-  routes {
-    simple_route {
-      http_method = "ANY"
-      path {
-        prefix = "/"
-      }
-      headers {
-        name         = "WWW-Authenticate"
-        regex        = "(.*[nN][tT][lL][mM].*)"
-        invert_match = false
-      }
-      origin_pools {
-        pool {
-          namespace = var.namespace
-          name      = volterra_origin_pool.origin.name
-        }
-        weight   = 1
-        priority = 1
-      }
-      advanced_options {
-        priority       = "DEFAULT"
-        prefix_rewrite = "/rewritten/"
-        app_firewall {
-          name      = "${var.name}-waap"
-          namespace = var.namespace
-        }
-        response_headers_to_remove = ["Proxy-support"]
-
-        response_headers_to_add {
-          name   = "WWW-Authenticate"
-          value  = "Negotiate"
-          append = false
-        }
-      }
-    }
-  }
-
-  routes {
-    simple_route {
-      http_method = "GET"
-      path {
-        regex = ".*(.jpg|.png|.gif)"
-      }
-      origin_pools {
-        pool {
-          namespace = var.namespace
-          name      = volterra_origin_pool.origin.name
-        }
-        weight   = 1
-        priority = 1
-      }
-    }
-  }
-
 }
