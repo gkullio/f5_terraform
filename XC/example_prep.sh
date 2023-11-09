7f2464025fae79e719c6821ba3f5dd78dac88a76
gkullio <105679126+gkullio@users.noreply.github.com>
2023-11-08 21:12:33 -0800

    Delete XC directory
    Emergency

--- XC/example_prep.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-###If you are using WSL cli then you will have to manually add the export statements in your ~./bash_profile
-
-export VOLT_API_P12_FILE=/api-creds.p12
-export VES_P12_PASSWORD=123456789
--- XC/xc/provider-xcs.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = ">= 0.12"
-  required_providers {
-    volterra = {
-      source  = "volterraedge/volterra"
-      version = "0.11.19"
-    }
-  }
-}
