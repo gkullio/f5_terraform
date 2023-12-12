61b8338fa3ab40f3fe8e37391ead6d41f0de07a8
Gage Kulland <g.kulland@f5.com>
2023-12-12 14:13:18 -0800

    changes

---
+++ BIG-IP Public Cloud/Azure/Standalone/1-NIC/BYOL_New_vNet_1NIC/outputs.tf
@@ -0,0 +1,91 @@
+#  Network Outputs
+
+output "resource_group" {
+  description = "Resource group name"
+  value       = azurerm_resource_group.main.name
+}
+output "vnet" {
+  description = "VNet name"
+  value       = azurerm_virtual_network.main.name
+}
+output "mgmt_subnet_addresses" {
+  description = "Management subnet address prefix"
+  value       = azurerm_subnet.mgmt.address_prefixes
+}
+output "mgmt_subnet_name" {
+  description = "Management subnet name"
+  value       = azurerm_subnet.mgmt.name
+}
+output "mgmt_security_group_name" {
+  description = "Management security group name"
+  value       = azurerm_network_security_group.mgmt.name
+}
+output "external_subnet_addresses" {
+  description = "External subnet address prefix"
+  value       = azurerm_subnet.external.address_prefixes
+}
+output "external_subnet_name" {
+  description = "External subnet name"
+  value       = azurerm_subnet.external.name
+}
+output "external_security_group_name" {
+  description = "External security group name"
+  value       = azurerm_network_security_group.external.name
+}
+output "internal_subnet_addresses" {
+  description = "Internal subnet address prefix"
+  value       = azurerm_subnet.internal.address_prefixes
+}
+output "internal_subnet_name" {
+  description = "Internal subnet name"
+  value       = azurerm_subnet.internal.name
+}
+output "internal_security_group_name" {
+  description = "Internal security group name"
+  value       = azurerm_network_security_group.internal.name
+}
+
+
+
+# BIG-IP Outputs
+
+output "f5vm01_mgmt_private_ip" {
+  description = "f5vm01 management private IP address"
+  value       = module.bigip.private_addresses["mgmt_private"]["private_ip"][0]
+}
+output "f5vm01_mgmt_public_ip" {
+  description = "f5vm01 management public IP address"
+  value       = module.bigip.mgmtPublicIP
+}
+output "f5vm01_mgmt_pip_url" {
+  description = "f5vm01 management public URL"
+  value       = "https://${module.bigip.mgmtPublicIP}"
+}
+output "f5vm01_ext_private_ip" {
+  description = "f5vm01 external primary IP address (self IP)"
+  value       = module.bigip.private_addresses["public_private"]["private_ip"][0]
+}
+output "f5vm01_ext_public_ip" {
+  description = "f5vm01 external public IP address (self IP)"
+  value       = module.bigip.public_addresses["external_primary_public"][0]
+}
+output "f5vm01_ext_secondary_ip" {
+  description = "f5vm01 external secondary IP address (VIP)"
+  value       = local.vm01_vip_ips.app1.ip
+}
+output "f5vm01_int_private_ip" {
+  description = "f5vm01 internal primary IP address"
+  value       = module.bigip.private_addresses["internal_private"]["private_ip"][0]
+}
+output "f5vm01_instance_ids" {
+  description = "f5vm01 management device name"
+  value       = module.bigip.bigip_instance_ids
+}
+output "public_vip" {
+  description = "Public IP for the BIG-IP listener (VIP)"
+  value       = module.bigip.public_addresses["external_secondary_public"][0]
+}
+output "public_vip_url" {
+  description = "public URL for application"
+  value       = "http://${module.bigip.public_addresses["external_secondary_public"][0]}"
+}
---
+++ BIG-IP Public Cloud/Azure/Standalone/1-NIC/BYOL_New_vNet_1NIC/variables.tf
@@ -0,0 +1,208 @@
+#### NOTE ####
+# All variables are defined in the variables.tf file.  
+#This file is used to define the variables and their descriptions.  
+#The values for the variables are defined in the terraform.tfvars file.  
+#The terraform.tfvars file is not included in this repository for security reasons.  
+#The terraform.tfvars file should be created in the same directory as the variables.tf file.  
+#The terraform.tfvars file should contain the following variables and their values.
+#### End of NOTE ####
+
+# Variables
+
+variable "adminSrcAddr" {
+  type        = string
+  description = "Allowed Admin source IP prefix"
+  #Recommend using icanhazip.com to get your public IP address
+  #Recommend against 0.0.0.0/0 for security reasons  
+}
+variable "vnet_cidr" {
+  type        = string
+  description = "CIDR IP Address range of the Virtual Network"
+}
+variable "mgmt_address_prefix" {
+  type        = string
+  description = "Management subnet address prefix"
+}
+variable "ext_address_prefix" {
+  type        = string
+  description = "External subnet address prefix"
+}
+variable "int_address_prefix" {
+  type        = string
+  description = "Internal subnet address prefix"
+}
+variable "projectPrefix" {
+  type        = string
+  description = "This value is inserted at the beginning of each Azure object (alpha-numeric, no special character)"
+}
+variable "location" {
+  type        = string
+  description = "Azure Location of the deployment"
+}
+variable "availability_zone" {
+  type        = number
+  description = "Azure Availability Zone for BIG-IP 1"
+}
+variable "client_secret" {
+  type        = string
+  description = "This is the client secret for the Service Principal"
+}
+variable "client_id" {
+  type        = string
+  description = "This is the client id for the Service Principal"
+}
+variable "tenant_id" {
+  type        = string
+  description = "This is the tenant id for the Service Principal"
+}
+variable "subscription_id" {
+  type        = string
+  description = "This is the subscription id for the Service Principal"
+}
+variable "instance_type" {
+  type        = string
+  default     = "Standard_DS4_v2"
+  description = "Azure instance type to be used for the BIG-IP VE"
+}
+variable "image_name" {
+  type        = string
+  description = "F5 SKU (image) to deploy. Note: The disk size of the VM will be determined based on the option you select.  **Important**: If intending to provision multiple modules, ensure the appropriate value is selected, such as ****AllTwoBootLocations or AllOneBootLocation****."
+}
+variable "product" {
+  type        = string
+  description = "Azure BIG-IP VE Offer"
+}
+variable "bigip_version" {
+  type        = string
+  description = "BIG-IP Version"
+}
+variable "f5_username" {
+  type        = string
+  description = "User name for the BIG-IP"
+}
+variable "f5_password" {
+  type        = string
+  description = "BIG-IP Password or Key Vault secret name (value should be Key Vault secret name when az_key_vault_authentication = true, ex. my-bigip-secret)"
+}
+variable "az_keyvault_authentication" {
+  type        = bool
+  description = "Whether to use key vault to pass authentication"
+}
+variable "keyvault_rg" {
+  type        = string
+  description = "The name of the resource group in which the Azure Key Vault exists"
+}
+variable "keyvault_name" {
+  type        = string
+  description = "Name of Key Vault"
+}
+variable "keyvault_secret" {
+  type        = string
+  description = "Name of Key Vault secret with BIG-IP password"
+}
+variable "user_identity" {
+  type        = string
+  description = "The ID of the managed user identity to assign to the BIG-IP instance"
+}
+variable "ssh_key" {
+  type        = string
+  description = "public key used for authentication in /path/file format (e.g. /.ssh/id_rsa.pub)"
+}
+variable "license1" {
+  type        = string
+  description = "The license token for the 1st F5 BIG-IP VE (BYOL)"
+}
+variable "dns_server" {
+  type        = string
+  description = "Leave the default DNS server the BIG-IP uses, or replace the default DNS server with the one you want to use"
+}
+variable "dns_suffix" {
+  type        = string
+  description = "DNS suffix for your domain in the GCP project"
+}
+variable "ntp_server" {
+  type        = string
+  description = "Leave the default NTP server the BIG-IP uses, or replace the default NTP server with the one you want to use"
+}
+variable "timezone" {
+  type        = string
+  default     = "UTC"
+  description = "If you would like to change the time zone the BIG-IP uses, enter the time zone you want to use. This is based on the tz database found in /usr/share/zoneinfo (see the full list [here](https://github.com/F5Networks/f5-azure-arm-templates/blob/master/azure-timezone-list.md)). Example values: UTC, US/Pacific, US/Eastern, Europe/London or Asia/Singapore."
+}
+variable "DO_URL" {
+  type        = string
+  description = "URL to download the BIG-IP Declarative Onboarding module"
+}
+variable "AS3_URL" {
+  type        = string
+  description = "URL to download the BIG-IP Application Service Extension 3 (AS3) module"
+}
+variable "TS_URL" {
+  type        = string
+  description = "URL to download the BIG-IP Telemetry Streaming module"
+}
+variable "FAST_URL" {
+  type        = string
+  description = "URL to download the BIG-IP FAST module"
+}
+variable "INIT_URL" {
+  type        = string
+  description = "URL to download the BIG-IP runtime init"
+}
+variable "libs_dir" {
+  type        = string
+  description = "Directory on the BIG-IP to download the A&O Toolchain into"
+}
+variable "bigIqHost" {
+  type        = string
+  default     = ""
+  description = "This is the BIG-IQ License Manager host name or IP address"
+}
+variable "bigIqUsername" {
+  type        = string
+  default     = "azureuser"
+  description = "Admin name for BIG-IQ"
+}
+variable "bigIqPassword" {
+  type        = string
+  default     = "Default12345!"
+  description = "Admin Password for BIG-IQ"
+}
+variable "bigIqLicenseType" {
+  type        = string
+  default     = "licensePool"
+  description = "BIG-IQ license type"
+}
+variable "bigIqLicensePool" {
+  type        = string
+  default     = ""
+  description = "BIG-IQ license pool name"
+}
+variable "bigIqSkuKeyword1" {
+  type        = string
+  default     = "key1"
+  description = "BIG-IQ license SKU keyword 1"
+}
+variable "bigIqSkuKeyword2" {
+  type        = string
+  default     = "key2"
+  description = "BIG-IQ license SKU keyword 2"
+}
+variable "bigIqUnitOfMeasure" {
+  type        = string
+  default     = "hourly"
+  description = "BIG-IQ license unit of measure"
+}
+variable "bigIqHypervisor" {
+  type        = string
+  default     = "azure"
+  description = "BIG-IQ hypervisor"
+}
+variable "resourceOwner" {
+  type        = string
+  default     = null
+  description = "This is a tag used for object creation. Example is last name."
+}
+variable "vm_name" {
+  description = "Name of 1st BIG-IP. If empty, default is 'bigip1' string + prefix + random_id"
+}
