7fd66fe2df9522b23450126b02f155de641f77de
Gage Kulland <g.kulland@f5.com>
2024-06-08 08:03:48 -0700

    New aks terraform files7

---
+++ NGINX/Azure/Dev/AKS_k8s/aks.tf
@@ -0,0 +1,38 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+resource "random_pet" "prefix" {}
+
+resource "azurerm_resource_group" "default" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = {
+    environment = var.environment
+    email       = var.email
+    owner       = var.owner
+  }
+}
+
+resource "azurerm_kubernetes_cluster" "default" {
+  name                = "${random_pet.prefix.id}-aks"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+  dns_prefix          = "${random_pet.prefix.id}-k8s"
+  kubernetes_version  = "1.26.3"
+
+  default_node_pool {
+    name            = "default"
+    node_count      = 2
+    vm_size         = "Standard_D2_v2"
+    os_disk_size_gb = 30
+  }
+
+  role_based_access_control_enabled = true
+
+  tags = {
+    environment = var.environment
+    email       = var.email
+    owner       = var.owner
+  }
+}
---
+++ NGINX/Azure/Dev/AKS_k8s/variables.tf
@@ -0,0 +1,15 @@
+###### Tag variables ######
+variable "environment" {}
+variable "email" {}
+variable "owner" {}
+
+###### Global Variables ######
+variable "resource_group_name"  {}
+variable "location"  {}
+
+###### Azure Credentials ######
+variable "client_id"  {}
+variable "client_secret"  {}
+variable "tenant_id"  {}
+variable "subscription_id"  {}
+
